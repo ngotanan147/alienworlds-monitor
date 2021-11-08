a0aed1ce08746fc1077865460e86d840c23befd4
ngotanan147 <ngotanan147@gmail.com>
2021-11-08 17:23:03 +0700

    up

--- src/backend/axios.js
+++ src/backend/axios.js
@@ -2,10 +2,16 @@ const axios = require('axios')
 const rateLimit = require('axios-rate-limit')
 const axiosRetry = require('axios-retry')
 
-const http = rateLimit(axios.create(), { maxRequests: 1000, perMilliseconds: 1000, maxRPS: 1000 })
+const http = rateLimit(axios.create(), {
+    maxRequests: 1000, perMilliseconds: 1000, maxRPS: 1000,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    }
+})
 
 axiosRetry(http, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
-console.log("Server Axios RPS: "+http.getMaxRPS())
+console.log("Server Axios RPS: " + http.getMaxRPS())
 
 module.exports = http;
